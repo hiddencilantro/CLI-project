0db805bd7ba633b3ee4b55bf3783fb1f89495415
hiddencilantro <stevekim1109@gmail.com>
2020-10-22 05:12:19 +0900

    Done with initial first and second scrape (in run file)

--- lib/players.rb
+++ lib/players.rb
@@ -1,9 +1,13 @@
 class Players
     
-    def initialize(number, name, hometown)
+    def initialize(number, name, hometown, birthdate, player_detail_divided_text1, player_detail_divided_text2)
         @number = number
         @name = name
         @hometown = hometown
+        @birthdate = birthdate
+        @player_detail_divided_text1 = player_detail_divided_text1
+        @player_detail_divided_text2 = player_detail_divided_text2
+        # @player_detail_divided_text3 = player_detail_divided_text3
     end
 
 end
